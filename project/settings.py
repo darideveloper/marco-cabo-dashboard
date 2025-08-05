809759ed255e9bd0199274238c42fac7ab586a1a
daridev <darideveloper@gmail.com>
2025-08-05 16:14:09 -0600

    chore (jazzmin): change model order (start with travels.Vehicle)

--- project/settings.py
+++ project/settings.py
@@ -218,13 +218,13 @@ JAZZMIN_SETTINGS = {
     # List of apps (and/or models) to base side menu ordering off of
     # (does not need to contain all apps/models)
     "order_with_respect_to": [
+        "travels.Vehicle",
         "travels.Zone",
         "travels.Location",
         "travels.Pricing",
         "travels.TransferType",
         "travels.Client",
         "travels.VipCode",
-        "travels.Vehicle",
     ],
 
     # Custom links to append to app groups, keyed on app name
