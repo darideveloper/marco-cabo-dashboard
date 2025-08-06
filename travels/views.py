ce0c33942365f381f5d3a606730e13907f9b55a7
daridev <darideveloper@gmail.com>
2025-08-06 15:37:56 -0600

    chore (travels/views): sort imports

--- travels/views.py
+++ travels/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from django_filters.rest_framework import DjangoFilterBackend
 
 from travels import models
