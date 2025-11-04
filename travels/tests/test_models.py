bdb885268d162809ea9581fbf52c3b09da266a75
daridev <darideveloper@gmail.com>
2025-11-04 17:43:55 -0600

    test (SaleTestCase): validate auto generated stripe code

--- travels/tests/test_models.py
+++ travels/tests/test_models.py
@@ -1,5 +1,6 @@
-from travels import models
+import uuid
 
+from travels import models
 from core.tests_base.test_models import TestTravelsModelBase
 
 
@@ -41,3 +42,11 @@ class SaleTestCase(TestTravelsModelBase):
         self.assertIn(sale.location.name, summary)
         self.assertIn(sale.service_type.name, summary)
         self.assertIn(str(sale.total), summary)
+        
+    def test_save_auto_create_stripe_code(self):
+        """Test sale save auto create stripe code"""
+
+        # Create a sale
+        sale = self.create_sale()
+        self.assertIsNotNone(sale.stripe_code)
+        self.assertTrue(isinstance(sale.stripe_code, uuid.UUID))
