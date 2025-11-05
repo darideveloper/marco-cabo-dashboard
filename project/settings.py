d9ad642058cb57e64b2e43042f312cae59715088
daridev <darideveloper@gmail.com>
2025-11-05 10:20:44 -0600

    feat (settings/env): add LANDING_HOST_COMPLETE_BOOKING and default value in LANDING_HOST

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ STORAGE_AWS = os.environ.get("STORAGE_AWS") == "True"
 HOST = os.getenv("HOST")
 TEST_HEADLESS = os.getenv("TEST_HEADLESS", "False") == "True"
 PAGE_SIZE = int(os.getenv("PAGE_SIZE", 10))
-LANDING_HOST = os.getenv("LANDING_HOST")
+LANDING_HOST = os.getenv("LANDING_HOST", "")
 LANDING_HOST_SUCCESS = LANDING_HOST + "/?status=done"
 LANDING_HOST_ERROR = LANDING_HOST + "/?status=error"
 LANDING_HOST_COMPLETE_BOOKING = LANDING_HOST + "/complete-booking/"
