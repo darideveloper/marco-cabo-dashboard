6598c2063cf35defd6fe3649b040c925141a0fcd
unknown <abelsotovaldez@gmail.com>
2025-11-05 22:37:31 -0600

    fix(travels/serializers): remove email from sale done serializer

--- travels/serializers.py
+++ travels/serializers.py
@@ -97,7 +97,6 @@ class SaleSerializer(serializers.Serializer):
 class SaleDoneSerializer(serializers.Serializer):
     sale_stripe_code = serializers.CharField(required=True)
     client_last_name = serializers.CharField(source="client.last_name")
-    client_email = serializers.EmailField(source="client.email")
     client_phone = serializers.CharField(source="client.phone")
     passengers = serializers.IntegerField(source="sale.passengers")
     details = serializers.CharField(
